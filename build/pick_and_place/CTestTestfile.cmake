5b350f5f2f66cbc494c2300e3ba99009e63f98c3
divyank <d.divvu.yerravarapu@gmail.com>
2024-08-21 15:25:29 +0100

    Add files via upload

---
+++ build/pick_and_place/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/iamgroot/pick_and_place_ws/src/pick_and_place
+# Build directory: /home/iamgroot/pick_and_place_ws/build/pick_and_place
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
